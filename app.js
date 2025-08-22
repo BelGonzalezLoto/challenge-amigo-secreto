2507eb412a9969ddfafe72fc2be16dd23ece0f3f
Belén Gonzalez <maribelengl@gmail.com>
2025-08-22 20:31:51 -0300

    feat: agregar variables globales y expresión regular para validación

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
- 
+// Variable de tipo array para almacenar nombres de amigos 
+let amigos = [];
+
+// Expresión para validar que solo contenga letras y espacios 
+const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/; 
